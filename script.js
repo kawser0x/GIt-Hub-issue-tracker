aec201990d9f0fcb2375e57eaa1ddcb05ae0b45c
kawser0x <kawserswe@gmail.com>
2026-03-29 03:26:13 +0600

    count issuess and all button work also card top color

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const showIssue = document.getElementById("showIssue");
 
 const displycontainer = document.getElementById("show-issues");
 
+const colorP = document.getElementById("colorP");
+
+const countIssues = document.getElementById("countIssue");
+
 loginbtn.addEventListener("click", () => {
   if (admininput.value === "admin" && logPass.value === "admin123") {
     login.classList.add("hidden");
@@ -16,77 +20,70 @@ loginbtn.addEventListener("click", () => {
   }
 });
 
-// "id": 7,
-// "title": "Improve search functionality",
-// "description": "Add filters for advanced search including date ranges, status, and tags.",
-// "status": "open",
-// "labels": [
-// "enhancement",
-// "good first issue"
-// ],
-// "priority": "low",
-// "author": "search_guru",
-// "assignee": "emma_ui",
-// "createdAt": "2024-01-17T12:00:00Z",
-// "updatedAt": "2024-01-17T12:00:00Z"
-
 function findP(priority) {
-    const p = priority.toLowerCase();
-    
-    if (p === "high") {
-        return `
-            <img class="w-8" src="./assets/Open-Status.png" alt="status" />
-            <span class="btn btn-outline btn-secondary rounded-full uppercase font-bold text-xs">${priority}</span>
-        `;
-    } 
-    else if (p === "medium") { 
-        return `
-            <img class="w-8" src="./assets/Open-Status.png" alt="status" />
-            <span class="btn btn-outline btn-warning rounded-full uppercase font-bold text-xs">${priority}</span>
-        `;
-    } 
-    else {
-        return `
-            <img class="w-8" src="./assets/Closed- Status .png" alt="status" />
-            <span class="btn btn-outline rounded-full uppercase font-bold text-xs">${priority}</span>
-        `;
-    }
+  const p = priority.toLowerCase();
+  let html = "";
+  let colorClass = "";
+
+  if (p === "high") {
+    html = `
+      <img class="w-8" src="./assets/Open-Status.png" alt="status" />
+      <span class="btn btn-outline btn-secondary rounded-full uppercase font-bold text-xs">${priority}</span>
+    `;
+    colorClass = "border-green-400";
+  } else if (p === "medium") {
+    html = `
+      <img class="w-8" src="./assets/Open-Status.png" alt="status" />
+      <span class="btn btn-outline btn-warning rounded-full uppercase font-bold text-xs">${priority}</span>
+    `;
+    colorClass = "border-green-400";
+  } else {
+    html = `
+      <img class="w-8" src="./assets/Closed- Status .png" alt="status" />
+      <span class="btn btn-outline rounded-full uppercase font-bold text-xs">${priority}</span>
+    `;
+    colorClass = "border-purple-400";
+  }
+
+  return { html, colorClass };
 }
 
-async function displayIssue(){
-    const response = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
-    const data = await response.json();
-    divContant(data.data);
+async function displayIssue() {
+  const response = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+  const data = await response.json();
+  divContant(data.data);
 }
 
 
 function divContant(data) {
+    countIssues.innerText = data.length;
   displycontainer.innerHTML = "";
-  
-  data.forEach(item => {
-    const card = document.createElement("div");
-    
-    const labelsHtml = item.labels.map(label => {
-      if (label.toLowerCase() === "bug") {
-        return `<button class="badge badge-outline badge-secondary"><i class="fa-solid fa-bug "></i>${label.toUpperCase()}</button>`;
-      } 
-      else if (label.toLowerCase() === "enhancement") {
-        return `<button class="badge badge-outline badge-accent"><i class="fa-solid fa-link"></i>${label.toUpperCase()}</button>`;
-      } 
-      else if (label.toLowerCase() === "help wanted") {
-        return `<button class="badge badge-outline badge-warning"><i class="fa-regular fa-life-ring"></i>${label.toUpperCase()}</button>`;
-      } 
-      else {
-        return `<button class="badge badge-outline badge-primary"><i class="fa-brands fa-git-alt"></i>${label.toUpperCase()}</button>`;
-      }
-    }).join("");
 
-    const priorityL = findP(item.priority) ;
+  data.forEach((item) => {
+    const card = document.createElement("div");
+
+    const labelsHtml = item.labels
+      .map((label) => {
+        if (label.toLowerCase() === "bug") {
+          return `<button class="badge badge-outline badge-secondary"><i class="fa-solid fa-bug "></i>${label.toUpperCase()}</button>`;
+        } else if (label.toLowerCase() === "enhancement") {
+          return `<button class="badge badge-outline badge-accent"><i class="fa-solid fa-link"></i>${label.toUpperCase()}</button>`;
+        } else if (label.toLowerCase() === "help wanted") {
+          return `<button class="badge badge-outline badge-warning"><i class="fa-regular fa-life-ring"></i>${label.toUpperCase()}</button>`;
+        } else {
+          return `<button class="badge badge-outline badge-primary"><i class="fa-brands fa-git-alt"></i>${label.toUpperCase()}</button>`;
+        }
+      })
+      .join("");
+
+    const priorityL = findP(item.priority);
     card.innerHTML = `
-      <div class="shadow-xl p-6 space-y-3 border-t-2 border-red-400 h-full flex flex-col justify-between">
+      <div id="colorP" class="shadow-xl p-6 space-y-3 border-t-2 ${priorityL.colorClass}  h-full flex flex-col justify-between">
         <div class="space-y-3">
           <div class="flex justify-between items-center">
-            ${priorityL}
+            ${priorityL.html}
           </div>
           
           <div>
@@ -108,9 +105,13 @@ function divContant(data) {
     displycontainer.appendChild(card);
   });
 }
-function showbtn(){
-    
-}
 
+
+const allbutton = document.getElementById("allbutton");
+
+allbutton.addEventListener("click",()=>{
+    displayIssue();
+})
 
 displayIssue();
+
