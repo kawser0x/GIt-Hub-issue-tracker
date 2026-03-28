a532651c5e194c0a9c27b9c4e2dcf0ca723760e0
kawser0x <kawserswe@gmail.com>
2026-03-29 05:45:25 +0600

    modal part work properly

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function divContant(data) {
 
     const priorityL = findP(item.priority);
     card.innerHTML = `
-      <div id="colorP" class="shadow-xl p-6 space-y-3 border-t-2 ${priorityL.colorClass}  h-full flex flex-col justify-between">
+      <div onclick="openissueModal(${item.id})" id="colorP" class="shadow-xl p-6 space-y-3 border-t-2 ${priorityL.colorClass}  h-full flex flex-col justify-between">
         <div class="space-y-3">
           <div class="flex justify-between items-center">
             ${priorityL.html}
@@ -148,7 +148,7 @@ function divContantopen(data) {
 
       const priorityL = findP(item.priority);
       card.innerHTML = `
-      <div id="colorP" class="shadow-xl p-6 space-y-3 border-t-2 ${priorityL.colorClass}  h-full flex flex-col justify-between">
+      <div onclick="openissueModal(${item.id}) id="colorP" class="shadow-xl p-6 space-y-3 border-t-2 ${priorityL.colorClass}  h-full flex flex-col justify-between">
         <div class="space-y-3">
           <div class="flex justify-between items-center">
             ${priorityL.html}
@@ -215,7 +215,7 @@ function divContantclosed(data) {
 
       const priorityL = findP(item.priority);
       card.innerHTML = `
-      <div id="colorP" class="shadow-xl p-6 space-y-3 border-t-2 ${priorityL.colorClass}  h-full flex flex-col justify-between">
+      <div onclick="openissueModal(${item.id}) id="colorP" class="shadow-xl p-6 space-y-3 border-t-2 ${priorityL.colorClass}  h-full flex flex-col justify-between">
         <div class="space-y-3">
           <div class="flex justify-between items-center">
             ${priorityL.html}
@@ -249,5 +249,86 @@ closebutton.addEventListener("click", () => {
   allbutton.classList.remove("btn-primary");
   displayIssueclosed(); 
 });
+
+
+async function openissueModal(id){
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+    const data = await res.json();
+
+    if (data.status === "success") {
+        displayModal(data.data);
+    }
+    
+}
+
+// "status": "success",
+// "message": "Issue fetched successfully",
+// "data": {
+// "id": 33,
+// "title": "Add bulk operations support",
+// "description": "Allow users to perform bulk actions like delete, update status on multiple items at once.",
+// "status": "open",
+// "labels": [
+// "enhancement"
+// ],
+// "priority": "low",
+// "author": "bulk_barry",
+// "assignee": "",
+// "createdAt": "2024-02-02T10:00:00Z",
+// "updatedAt": "2024-02-02T10:00:00Z"
+// }
+
+function displayModal(issue) {
+    const labelsHtml = issue.labels
+        .map((label) => {
+          if (label.toLowerCase() === "bug") {
+            return `<button class="badge badge-outline badge-secondary"><i class="fa-solid fa-bug "></i>${label.toUpperCase()}</button>`;
+          } else if (label.toLowerCase() === "enhancement") {
+            return `<button class="badge badge-outline badge-accent"><i class="fa-solid fa-link"></i>${label.toUpperCase()}</button>`;
+          } else if (label.toLowerCase() === "help wanted") {
+            return `<button class="badge badge-outline badge-warning"><i class="fa-regular fa-life-ring"></i>${label.toUpperCase()}</button>`;
+          } else {
+            return `<button class="badge badge-outline badge-primary"><i class="fa-brands fa-git-alt"></i>${label.toUpperCase()}</button>`;
+          }
+        })
+        .join("");
+    const modalContent = document.getElementById("modalDiv");
+
+    modalContent.innerHTML = `
+        <div class="space-y-4">
+            <div>
+                <h2 class="text-2xl font-bold">${issue.title}</h2>
+                <div class="flex gap-2 items-center mt-2">
+                    <span class="badge badge-success">${issue.status}</span>
+                    <p class="text-sm font-light">
+                        ${issue.status} by <span class="font-bold"> . ${issue.author}</span> •  ${new Date(issue.createdAt).toLocaleDateString()}
+                    </p> 
+                </div>
+            </div>
+
+            <div class="flex flex-wrap gap-2">
+                ${labelsHtml}
+            </div>
+
+            <p class="text-sm text-gray-600  bg-base-200 p-4 rounded-lg">
+                ${issue.description}
+            </p>
+
+            <div class="flex justify-between border-t pt-4">
+                <div>
+                    <p class="text-xs uppercase text-gray-400">Assignee</p>
+                    <h2 class="text-lg font-bold">${issue.assignee || 'Unassigned'}</h2>
+                </div>
+                <div class="text-right">
+                    <p class="text-xs uppercase text-gray-400">Priority</p>
+                    <span class="badge badge-error font-bold p-3">${issue.priority.toUpperCase()}</span>
+                </div>
+            </div>
+        </div>
+    `;
+
+    // Open the DaisyUI Modal
+    document.getElementById("showissuemodal").showModal();
+}
 
 displayIssue();
